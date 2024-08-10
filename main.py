66f7dc6aacbf558f2d656646d9e9118d41864a7e
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2024-08-10 21:16:08 +0300

    add: orm.py add async version

--- main.py
+++ main.py
@@ -3,41 +3,71 @@ import asyncio
 from time import sleep
 
 from core import SystemCoreSync, SystemCoreAsync
-from orm import SystemORMSync
+from orm import SystemORMSync, SystemORMAsync
 from models import Action, LogTable, Role
 
 async def main():
-    await SystemCoreAsync.create_tables()
-    await SystemCoreAsync.add_user("Safronov", "boss")
-    await SystemCoreAsync.add_user("Kostenko", "worker")
-    await SystemCoreAsync.show_users()
-    await SystemCoreAsync.insert_logs([
-        {"user_id": 1, "action": "enter"},
-        {"user_id": 2, "action": "enter"},
-        {"user_id": 1, "action": "quit"}
-    ])
-    await SystemCoreAsync.update_worker_data(user_id=1, new_role="worker")
-    await SystemCoreAsync.show_logs()
+    await SystemORMAsync.create_tables()
+    await SystemORMAsync.add_user("Safronov", Role.chief)
+    await SystemORMAsync.add_user("Kostenko", Role.worker)
+    await SystemORMAsync.add_user("Nesterenko", Role.user)
+    await SystemORMAsync.add_user("Lukacheva", Role.worker)
+    await SystemORMAsync.add_user("Borivova", Role.fired)
+
+    await SystemORMAsync.update_worker_data(2, new_surname="Popov", new_role=Role.deputy_chief)
+
+    await SystemORMAsync.insert_logs([
+                    LogTable(user_id=1, action = Action.enter),
+                    LogTable(user_id=2, action = Action.enter),
+                    LogTable(user_id=1, action = Action.quit)
+                    ])
+    sleep(5)
+    await SystemORMAsync.insert_logs([
+                    LogTable(user_id=2, action = Action.quit),
+                    LogTable(user_id=3, action = Action.enter),
+                    LogTable(user_id=5, action = Action.enter),
+                    LogTable(user_id=5, action = Action.quit),
+                    LogTable(user_id=4, action = Action.enter),
+                    LogTable(user_id=3, action = Action.quit)
+                    ])
+    await SystemORMAsync.show_users()
+    await SystemORMAsync.show_logs()
+    await SystemORMAsync.show_logs_by_user()
 
 asyncio.get_event_loop().run_until_complete(main())
 
-# s = SystemORMSync()
-# s.create_tables()
-# s.add_user("Safronov", Role.chief)
-# s.add_user("Kostenko", Role.worker)
-# s.add_user("Nesterenko", Role.user)
-# s.add_user("Lukacheva", Role.worker)
-# s.add_user("Borivova", Role.fired)
 
-# s.update_worker_data(2, new_surname="Popov", new_role=Role.deputy_chief)
+# async def main():
+#     await SystemCoreAsync.create_tables()
+#     await SystemCoreAsync.add_user("Safronov", "boss")
+#     await SystemCoreAsync.add_user("Kostenko", "worker")
+#     await SystemCoreAsync.show_users()
+#     await SystemCoreAsync.insert_logs([
+#         {"user_id": 1, "action": "enter"},
+#         {"user_id": 2, "action": "enter"},
+#         {"user_id": 1, "action": "quit"}
+#     ])
+#     await SystemCoreAsync.update_worker_data(user_id=1, new_role="worker")
+#     await SystemCoreAsync.show_logs()
 
-# s.insert_logs([
+# asyncio.get_event_loop().run_until_complete(main())
+
+# SystemORMSync.create_tables()
+# SystemORMSync.add_user("Safronov", Role.chief)
+# SystemORMSync.add_user("Kostenko", Role.worker)
+# SystemORMSync.add_user("Nesterenko", Role.user)
+# SystemORMSync.add_user("Lukacheva", Role.worker)
+# SystemORMSync.add_user("Borivova", Role.fired)
+
+# SystemORMSync.update_worker_data(2, new_surname="Popov", new_role=Role.deputy_chief)
+
+# SystemORMSync.insert_logs([
 #                 LogTable(user_id=1, action = Action.enter),
 #                 LogTable(user_id=2, action = Action.enter),
 #                 LogTable(user_id=1, action = Action.quit)
 #                 ])
 # sleep(5)
-# s.insert_logs([
+# SystemORMSync.insert_logs([
 #                 LogTable(user_id=2, action = Action.quit),
 #                 LogTable(user_id=3, action = Action.enter),
 #                 LogTable(user_id=5, action = Action.enter),
@@ -45,21 +75,21 @@ asyncio.get_event_loop().run_until_complete(main())
 #                 LogTable(user_id=4, action = Action.enter),
 #                 LogTable(user_id=3, action = Action.quit)
 #                 ])
-# s.show_users()
-# s.show_logs()
-# s.show_logs_by_user()
+# SystemORMSync.show_users()
+# SystemORMSync.show_logs()
+# SystemORMSync.show_logs_by_user()
 
 
 # s = SystemCoreSync()
 
-# s.create_tables()
-# s.add_user("Safronov", "boss")
-# s.add_user("Kostenko", "worker")
-# s.show_users()
-# s.insert_logs([
+# SystemCoreSync.create_tables()
+# SystemCoreSync.add_user("Safronov", "boss")
+# SystemCoreSync.add_user("Kostenko", "worker")
+# SystemCoreSync.show_users()
+# SystemCoreSync.insert_logs([
 #     {"user_id": 1, "action": "enter"},
 #     {"user_id": 2, "action": "enter"},
 #     {"user_id": 1, "action": "quit"}
 # ])
-# s.update_worker_data(user_id=1, new_role="worker")
-# s.show_logs()
+# SystemCoreSync.update_worker_data(user_id=1, new_role="worker")
+# SystemCoreSync.show_logs()
--- orm.py
+++ orm.py
@@ -2,7 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 from tabulate import tabulate
 
-from database import sync_engine, sync_session_factory, Base
+from database import sync_engine, async_engine, sync_session_factory, async_session_factory, Base
 from models import LogTable, Role, UsersTable
 
 
@@ -71,3 +71,73 @@ class SystemORMSync:
             if new_role:
                 user.role = new_role
             session.commit()
+
+
+class SystemORMAsync:
+
+    @staticmethod
+    async def create_tables():
+        async with async_engine.connect() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+            await conn.commit()
+
+    @staticmethod
+    async def insert_logs(logs: list[LogTable]):
+        async with async_session_factory() as session:
+            session.add_all(logs)
+            await session.commit()
+    
+    @staticmethod
+    async def show_logs(user_id: int = None):
+        async with async_session_factory() as session:
+            query = select(LogTable.id, 
+                        LogTable.user_id, 
+                        UsersTable.surname, 
+                        UsersTable.role, 
+                        LogTable.action, 
+                        LogTable.at).join(UsersTable, UsersTable.id == LogTable.user_id)
+            if user_id:
+                query = query.filter_by(id=user_id)
+            res = await session.execute(query)
+            res = res.all()
+            print(tabulate(res, headers=("id", "user_id", "surname", "role", "action", "at"), tablefmt="double_grid"))
+    
+    @staticmethod
+    async def show_logs_by_user():
+        async with async_session_factory() as session:
+            query = select(UsersTable).options(selectinload(UsersTable.logs))
+            result = await session.execute(query)
+            result = result.unique().scalars().all()
+            res = []
+            for user in result:
+                res += user.logs
+            res = [repr(log).split("|") for log in res]
+            print(tabulate(res, headers=("id", "user_id", "surname", "role", "action", "at"), tablefmt="double_grid"))
+    
+    @staticmethod
+    async def add_user(surname: str, role: Role):
+        async with async_session_factory() as session:
+            session.add(UsersTable(surname=surname, role=role))
+            await session.commit()
+
+    @staticmethod
+    async def show_users(user_id: int = None):
+        async with async_session_factory() as session:
+            query = select(UsersTable)
+            if user_id:
+                query = query.filter_by(id=user_id)
+            res = await session.execute(query)
+            res = res.scalars().all()
+            res = [repr(user).split("|") for user in res]
+            print(tabulate(res, headers=("id", "surname", "role"), tablefmt="double_grid"))
+    
+    @staticmethod
+    async def update_worker_data(user_id: int, new_surname: str = None, new_role: str = None):
+        async with async_session_factory() as session:
+            user = await session.get(UsersTable, user_id)
+            if new_surname:
+                user.surname = new_surname
+            if new_role:
+                user.role = new_role
+            await session.commit()
